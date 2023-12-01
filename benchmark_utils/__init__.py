b99d7951936471eda1d7e1789be437aada9d8d0e
xiaoyanh <xiaoyanhn@gmail.com>
2023-11-30 18:28:53 -0800

    update benchmark_utils

--- benchmark_utils/__init__.py
+++ benchmark_utils/__init__.py
@@ -2,3 +2,9 @@
 # the benchmark objective, datasets, and solvers. The folder should have the
 # name `benchmark_utils`, and code defined inside will be importable using
 # the usual import syntax
+
+# Import the function from the module
+from .prepare_data import prepare_data
+
+# Specify what should be imported when using wildcard import *
+__all__ = ['prepare_data']
